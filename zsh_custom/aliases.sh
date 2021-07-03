fdf7c843340a212bb5c076886c7a680cb31e1787
Edwin <edwinclement08@gmail.com>
2021-07-04 02:42:22 +0530

    Added a bunch of Aliases

--- zsh_custom/aliases.sh
+++ zsh_custom/aliases.sh
@@ -1,11 +1,15 @@
-alias gpg=gpg2 
+alias gpg=gpg2
 alias mysql=mycli
 
 # common stuff
 alias h='head -n '
 alias t='tail -n '
 alias c=cat
+alias l=ls
+alias g=grep
+alias e=emacsclient -c
 
+alias iso='date "+%Y-%m-%d" | pbcopy'
 
 # zsh/bash
 alias mv='mv -v'
@@ -34,8 +38,9 @@ alias v=vim
 alias tt=tmuxinator
 
 # Reload Configuration
-alias zz='. ~/.zshrc'
+alias zz='exec zsh'
 alias zz.a='. ~/conf/zsh_custom/aliases.sh; . ~/conf/zsh_custom/aliases_with_completion.sh'
+alias zz.b='. ~/.bs_secrets'
 alias tt="tmux source ~/.tmux.conf"
 
 # Edit Configuration
@@ -43,12 +48,19 @@ alias v.s="vim ~/.SpaceVim.d/init.toml"
 
 # Edit Configuration, and reload 
 alias v.t="vim ~/.tmux.conf; tt"
+alias v.v="vim ~/.config/nvim/init.vim"
 alias v.za="vim ~/conf/zsh_custom/aliases.sh; zz.a"
 alias v.zc="vim ~/conf/zsh_custom; zz"
 alias v.z="vim ~/.zshrc; zz"
+alias v.a="nvim ~/.alacritty.yml"
+alias v.b="nvim ~/.bs_secrets; zz.b"
 
 # Directory Aliases
 alias cdd="cd ~/Downloads"
+alias cdb="cd ~/bin"
+
+# Get Mac CPU temps
+alias get_cpu_temps='sudo powermetrics --samplers smc |grep -i "CPU die temperature"'
 
 # autovote
 alias autovote=aur-auto-vote edwinclement08
@@ -56,6 +68,12 @@ alias autovote=aur-auto-vote edwinclement08
 # ESP-IDF
 alias get_idf='. $HOME/esp/esp-idf/export.sh'
 alias get_tty="ls -l /dev/tty* | egrep -v 'tty(S|[0-9])'"
+
+
+# Quick Directory swap
+alias cdd="cd ~/Downloads"
+alias cdc="cd ~/coding"
+alias cd.n="cd ~/.config/nvim"
 
 # docker(& compose) stuff
 alias d.c='docker-compose up'
@@ -74,7 +92,8 @@ alias cat="bat"
 
 # lazy git
 alias lg="lazygit"
-alias ll="l -hl --sort=extension"
+alias ls="exa -lFh --group-directories-first --git --icons"
+alias ll="\ls"
 
 # lazy docker
 alias lzd='lazydocker'
@@ -96,7 +115,62 @@ function notify() {
   osascript -e "display notification \"$2\" with title \"$1\" sound name \"Glass\""
 }
 
+jq_tail() {
+  jq -cR '. as $raw | try fromjson catch $raw'
+}
 
+jq_strip_meta_data() {
+  jq -c '.timestamp=.meta.timestamp | del(.data,.meta) | . as $raw | try fromjson catch $raw'
+}
+
+
+# Browserstack aliases
+# Kubernetes shortcut
+exec_pod() {
+  kubectl exec -it $(kubectl get pods | fzf | awk '{ print $1 }') -- sh
+}
+
+get_k8s_keys() { 
+  server=`kubectl get pods | awk '{ print $1 }' | grep rails-local` && kubectl exec -it $server -- cat config/initializers/keys.yml
+}
+logs_k8s_rails_local() { 
+  server=`kubectl get pods | awk '{ print $1 }' | grep rails-local ` && kubectl exec -it $server -- tail -f log/production.log | grep -v "chitragupta_loss_check"
+}
+logs_k8s_rails_live() { 
+  server=`kubectl get pods | awk '{ print $1 }' | grep rails-live ` && kubectl exec -it $server -- tail -f log/production.log
+}
+
+# alias get_k8s_keys="kubectl exec -it $(kubectl get pods | awk '{ print $1 \}' | grep rails-local ) -- cat config/initializers/keys.yml"
+# alias logs_k8s_rails_local="kubectl exec -it $(kubectl get pods | awk '{ print $1 }' | grep rails-local ) -- tail -f log/production.log"
+# alias logs_k8s_rails_live="kubectl exec -it $(kubectl get pods | awk '{ print $1 }' | grep rails-live ) -- tail -f log/production.log"
+
+
+
+# Staging Repeater Logs
+alias logs_repeater_003="ssh repeater-staging-003 -- tail -f /ebs/node-repeater.log | jq_tail"
+alias logs_repeater_004="ssh repeater-staging-004 -- tail -f /ebs/node-repeater.log | jq_tail"
+
+filter_repeater() {
+  grep -v chitragupta_loss_check \
+  | grep -v "No authTokens found. Nothing to send to repeater disconnect" \
+  | grep -v "[sendMetricsToInvoker] Error:" \
+  | jq_strip_meta_data
+}
+
+# Invoke Local Binary for envs
+alias BrowserStackLocal_devlocal='BrowserStackLocal --key $BS_KEY_dev_local --bs-host k8s-devlocal.bsstag.com'
+alias BrowserStackLocal_prod='BrowserStackLocal --key $BS_KEY'
+
+
+#Get terminal color codes
+# for use in tmux and the like
+function get_term_colors() {
+  for i in {0..255}; do
+      printf "\x1b[38;5;${i}mcolour${i}\x1b[0m\n"
+  done
+}
+
+# TODO Add a conditional here to only load these in arch systems
 # Common use aliases
 alias aup="pamac upgrade --aur"
 alias grubup="sudo update-grub"
@@ -120,11 +194,16 @@ alias hw='hwinfo --short'                                   # Hardware Info
 alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'			# List amount of -git packages
 alias tb='nc termbin.com 9999'
 
-# Get fastest mirrors 
-alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist" 
-alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist" 
-alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist" 
-alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist" 
+# Get fastest mirrors
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
 
 
 alias get_installed_packages=" expac --timefmt='%Y-%m-%d %T' '%l\t%n' | sort | tail -n 10"
+
+timezsh() {
+  shell=${1-$SHELL}
+  for i in $(seq 1 10); do /usr/bin/time $shell -i -c exit; done
+}
