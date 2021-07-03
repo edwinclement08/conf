9b5420650978688a0a9306a2d62a79aa1055659f
Edwin <edwinclement08@gmail.com>
2021-07-04 02:43:28 +0530

    Installed go for mac

---
+++ zsh_custom/path_modifications.sh
@@ -0,0 +1,6 @@
+
+# Go configuration for Mac OS
+export GOPATH=$HOME/go
+#export GOROOT="$(brew --prefix golang)/libexec"
+export GOROOT="/usr/local/opt/go/libexec"
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
