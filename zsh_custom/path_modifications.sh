5a8c2e472083525e4897278f0bce42f8232a3714
Edwin <edwinclement08@gmail.com>
2021-07-07 11:38:16 +0530

    added doom emacs bin to the path

--- zsh_custom/path_modifications.sh
+++ zsh_custom/path_modifications.sh
@@ -4,3 +4,7 @@ export GOPATH=$HOME/go
 #export GOROOT="$(brew --prefix golang)/libexec"
 export GOROOT="/usr/local/opt/go/libexec"
 export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+
+export PATH="$HOME/.emacs.d/bin/:$PATH"
+
+
